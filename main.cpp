a3918d28902d098b8318d3191fd7483a52e31fde
danielebruno98 <daniele_bruno98@live.it>
2016-11-14 10:02:17 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ordinamento.h"
+
+
+int main() {
+	int i;
+int colonne[20];
+int righe[10];
+int numero;
+
+
+	return 0;
+}
---
+++ ordinamento.h
@@ -0,0 +1,22 @@
+int colonne[20];
+int righe[10];
+int matrice[righe][colonne];
+int i,j;
+
+for(int i=0; i<righe; i++)
+{
+
+for(int j=0; j<colonne; j++)
+{
+  numero = rand() % 101;
+  cout<<matrice[10][20];
+}
+}
+
+for(int i=0; i<righe; i++)
+{
+
+for(int i=0; i<colonne; i++)
+{
+  cout<<numero;
+}
